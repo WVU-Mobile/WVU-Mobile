e9e0e661ac304ba1bc8364231373460108389167
Kaitlyn Landmesser <kaitlynlandmesser@softwaredev-mac.local>
2016-12-28 14:58:58 -0500

    + these two files, idk whats wrong with them

--- WVU Mobile/PRTView.swift
+++ WVU Mobile/PRTView.swift
@@ -8,49 +8,9 @@
 
 import UIKit
 
-@IBDesignable class PRTView: UIView {
+@IBDesignable class PRTView: ShadowView {
 
-    @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
     @IBOutlet weak var icon: UIImageView!
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
 
-    @IBInspectable var cornerRadius: CGFloat = 5.0
-    
-    private var customBackgroundColor = UIColor.white
-
-    override func draw(_ rect: CGRect) {
-        customBackgroundColor.setFill()
-        UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius ).fill()
-    }
-
-    func press() {
-        self.layer.shadowOpacity = 0.1
-        self.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-        self.layer.shadowRadius = 3.0
-    }
-    
-    
-    override var backgroundColor: UIColor?{
-        didSet {
-            customBackgroundColor = backgroundColor!
-            super.backgroundColor = UIColor.clear
-        }
-    }
-    
-    func setup() {
-        layer.shadowOffset = CGSize.zero
-        layer.shadowRadius = 5.0
-        layer.shadowOpacity = 0.2
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.setup()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.setup()
-    }
 }
