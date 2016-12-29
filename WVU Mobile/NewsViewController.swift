d295f552d78607f67fd1a1e5b0701c485ed413c5
Kaitlyn Landmesser <kaitlynlandmesser@softwaredev-mac.local>
2016-12-29 11:21:16 -0500

    Created a refresh view for news + added a dispatch group for loading from multiple RSS feeds.

--- WVU Mobile/NewsViewController.swift
+++ WVU Mobile/NewsViewController.swift
@@ -10,59 +10,60 @@ import UIKit
 
 class NewsViewController: UITableViewController {
     var news = [RSSElement]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.refreshControl?.beginRefreshing()
+        pullNews()
+        
+        self.refreshControl?.addTarget(self, action: #selector(pullNews), for: UIControlEvents.valueChanged)
+    }
+    
+    func pullNews() {
+        news = []
+        let group = DispatchGroup()
+        
         let request = RSSRequest()
-
-        DispatchQueue.global().async {
+        DispatchQueue.global().async(group: group) {
             request.getNews(source: .wvutoday, completion: { news in
                 DispatchQueue.main.sync {
-                    
                     self.news += news
-                    
-                    self.news.sort(by: {$0.date.timeIntervalSince1970 > $1.date.timeIntervalSince1970})
-                    
-                    self.tableView.reloadData()
-        
                 }
             })
         }
         
         let request2 = RSSRequest()
-
-        DispatchQueue.global().async {
+        DispatchQueue.global().async(group: group) {
             request2.getNews(source: .da, completion: { news in
                 DispatchQueue.main.sync {
-                    
                     self.news += news
-
-                    self.news.sort(by: {$0.date.timeIntervalSince1970 > $1.date.timeIntervalSince1970})
-
-                    self.tableView.reloadData()
-                    
                 }
             })
         }
-        // sort by date
+        
+        group.notify(queue: DispatchQueue.main) {
+            self.news.sort(by: {$0.date.timeIntervalSince1970 > $1.date.timeIntervalSince1970})
+            self.tableView.reloadData()
+            self.refreshControl?.endRefreshing()
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return news.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let imageData = news[indexPath.row].image {
@@ -115,7 +116,7 @@ class NewsViewController: UITableViewController {
         
         self.navigationController?.pushViewController(webView, animated: true)
     }
-
+    
     override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableViewAutomaticDimension
     }
