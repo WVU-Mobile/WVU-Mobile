86e36d84cad65e3853eace662e01ddaf129520b4
Kaitlyn Reneé Landmesser <kaitlynrenee@Kaitlyns-MacBook.local>
2018-03-25 20:35:37 -0400

    Commiting pods?

---
+++ Pods/Target Support Files/Pods-PRT Status Widget/Pods-PRT Status Widget-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PRT_Status_Widget : NSObject
+@end
+@implementation PodsDummy_Pods_PRT_Status_Widget
+@end
---
+++ Pods/Target Support Files/Pods-WVU Mobile/Pods-WVU Mobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WVU_Mobile : NSObject
+@end
+@implementation PodsDummy_Pods_WVU_Mobile
+@end
